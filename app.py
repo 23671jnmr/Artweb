c38f8ee9a58363a135455b0d13cecc9208ddde50
23671jnmr <23671@burnside.school.nz>
2026-05-20 10:25:29 +1200

    setting up css so its not messy -unfinished

--- app.py
+++ app.py
@@ -8,6 +8,26 @@ def home():
     # home page
     return render_template('home.html')
 
+@app.route('/login.html')
+def login():
+    # login page
+    return render_template('login.html')
+
+@app.route('/traditional.html')
+def traditional():
+    # traditional page
+    return render_template('traditional.html')
+
+@app.route('/digital.html')
+def digital():
+    # digital page
+    return render_template('digital.html')
+
+@app.route('/others.html')
+def other():
+    #other page
+    return render_template('others.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
