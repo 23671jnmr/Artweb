a4e4ecfbeeca59ca0b9e6b1503c05c966ab610a3
23671jnmr <23671@burnside.school.nz>
2026-06-11 14:19:34 +1200

    making comments table

--- app.py
+++ app.py
@@ -3,20 +3,24 @@ import sqlite3
 
 app = Flask(__name__)
 
+
 def get_db():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
 
 @app.route('/')
 def home():
     # home page
     return render_template('home.html')
 
+
 @app.route('/login.html')
 def login():
     # login page
     return render_template('login.html')
+
 
 @app.route('/traditional.html')
 def traditional():
@@ -33,15 +37,14 @@ def digital():
     # digital page
     return render_template('digital.html')
 
+
 @app.route('/others.html')
 def other():
-    #other page
+    #other page#
     return render_template('others.html')
 
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-#sign up 
-
-    
+#sign up
